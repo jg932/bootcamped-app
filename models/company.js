a2fb0f8acc5953e751d61d6413016d42f9687314
Abdul2067 <abdul2067@gmail.com>
2021-12-17 11:30:42 -0500

    create company model

---
+++ models/company.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose"
+
+
+const Schema = mongoose.Schema
+
+const companySchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  website: {
+    type: URL,
+  },
+  structure: {
+    type: String
+  },
+  user:[{type: Schema.Types.ObjectId, ref:"User"}]
+})
+
+
+const Company = mongoose.model("Company", companySchema)
+
+
+export {
+  Company
+}
