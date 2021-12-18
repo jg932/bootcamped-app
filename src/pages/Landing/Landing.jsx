6252daa05a83cdc57fad03c462b2896acdf85634
jg932 <jared.h.goldstein@gmail.com>
2021-12-18 16:36:39 -0500

    animation on landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,6 +1,7 @@
 import styles from './Landing.module.css'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import * as LottiePlayer from "@lottiefiles/lottie-player";
 
 //Assets
 import Logo from '../../assets/images/logo.jpeg'
@@ -11,7 +12,15 @@ const Landing = () => {
     <div className="landing-page">
       <div className="landing-elements">
         <img className="logo-splash" src={Logo} alt="bootcamped logo"></img>
-        <img className="mobile-logo" src={      } alt=""></img>
+        <lottie-player
+          autoplay
+          loop
+          mode="normal"
+          src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"
+          width="300px"
+          background="transparent"
+          speed="1"
+        ></lottie-player>
         <div className="landing-button-container">
           <button><Link to="/signup">Sign Up</Link></button>
           <button><Link to="/signin">Sign In</Link></button>
