2c25c63c6cbbad8d635f8b96781f8b5eef6a04aa
kmcabano <kmcabano@gmail.com>
2021-12-18 16:36:09 -0500

    stub companies components & routing

---
+++ src/pages/Companies/CompanyCard.jsx
@@ -0,0 +1,12 @@
+import styles from './Companies.module.css'
+import { Link } from 'react-router-dom'
+
+const CompanyCard = () => {
+  return (
+    <>
+      <h2>[Company Card here]</h2>
+    </>
+  )
+}
+
+export default CompanyCard
---
+++ src/pages/Companies/HiringInfo.jsx
@@ -0,0 +1,8 @@
+import styles from './Companies.module.css'
+import { Link } from 'react-router-dom'
+
+const HiringInfo = () => {
+  return (
+    <h1>[Hiring Process Info here]</h1>
+  )
+}
