afbd4a30f07da49ed68b6712dd6735ec7f932563
kmcabano <kmcabano@gmail.com>
2021-12-18 16:43:10 -0500

    stub up listing components/routes

--- src/pages/Listings/Listings.jsx
+++ src/pages/Listings/Listings.jsx
@@ -1,8 +1,14 @@
 import styles from './Listings.module.css'
+import { Link } from 'react-router-dom'
+import ListingCard from './ListingCard'
 
 const Listings = () => {
   return (
-    <h1>Job listings will go here</h1>
+    <>
+      <h1>[Job Listings here]</h1>
+      <Link to="/listings/create">Create new listing</Link>
+    </>
+    
   )
 }
 
