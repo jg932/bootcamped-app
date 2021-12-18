73d5dde7c1b1f1fbecf3da09a361e70665eb54bf
Jrandle10 <91491549+Jrandle10@users.noreply.github.com>
2021-12-17 19:57:53 -0700

    complete create function

--- controllers/companies.js
+++ controllers/companies.js
@@ -1,8 +1,21 @@
+import { Profile } from '../models/profile.js'
 import { Company } from '../models/company.js'
 
 
 const create = async (req, res) => {
- console.log('creating companies'); 
+ console.log('creating companies')
+ try {
+   req.body.added_by = req.user.profile
+   const company = await new Company(req.body)
+   await company.save()
+   await Profile.updateOne(
+     { _id: req.user.profile },
+     { $push: { companies: company } }
+   )
+   return res.status(201).json(company)
+ } catch (err) {
+  return res.status(500).json(err)
+ }
 }
 
 
