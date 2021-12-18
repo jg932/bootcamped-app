64338f77e5afe0b89987752bdf3c54b23822940b
jg932 <jared.h.goldstein@gmail.com>
2021-12-18 15:37:39 -0500

    landing page logo

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,11 +2,15 @@ import styles from './Landing.module.css'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+//Assets
+import Logo from '../../assets/images/logo.jpeg'
+
+
 const Landing = () => {
   return (
     <div className="landing-page">
       <div className="landing-elements">
-        <img className="logo-splash" src={      } alt=""></img>
+        <img className="logo-splash" src={Logo} alt="bootcamped logo"></img>
         <img className="mobile-logo" src={      } alt=""></img>
         <div className="landing-button-container">
           <button><Link to="/signup">Sign Up</Link></button>
