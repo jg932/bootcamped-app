001b261599527a450e8e553aee24b73084ea0a3e
kmcabano <kmcabano@gmail.com>
2021-12-19 18:01:25 -0500

    begin create company page/form

---
+++ src/pages/Companies/CreateCompany.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import CompanyForm from "./CompanyForm";
+import Header from "../../components/Header";
+
+const CreateCompany = (props) => {
+  const navigate = useNavigate()
+  return (
+    <h1>[Create Company Page here]</h1>
+  )
+}
+
+export default CreateCompany
