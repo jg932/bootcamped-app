bb1a1a5b61c925b96cd9b930360fdb2c50d8ca6d
jg932 <jared.h.goldstein@gmail.com>
2021-12-20 11:34:38 -0500

    listing service init

--- src/pages/Listings/CreateListing.jsx
+++ src/pages/Listings/CreateListing.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import './Listings.module.css'
 
 //Components
 import ListingForm from './ListingForm.jsx'
+import Header from '../../components/Header.jsx'
+
 
 const CreateListing = (props) => {
 
   return (
     <div className="layout">
+      <Header />
       <ListingForm />
     </div>
   )
 }
 
-export default CreatePost
+export default CreateListing
