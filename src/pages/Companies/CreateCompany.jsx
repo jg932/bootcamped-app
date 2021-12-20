a9aa32aeb803a05b37ffdf050367cd60ac5d95e8
kmcabano <kmcabano@gmail.com>
2021-12-20 10:46:58 -0500

    begin company form -- name, location

--- src/pages/Companies/CreateCompany.jsx
+++ src/pages/Companies/CreateCompany.jsx
@@ -4,10 +4,49 @@ import { useNavigate } from "react-router-dom";
 import CompanyForm from "./CompanyForm";
 import Header from "../../components/Header";
 
+import { createCompany } from "../../services/companyService";
+
 const CreateCompany = (props) => {
   const navigate = useNavigate()
+  const [name, setName] = useState('')
+  const [location, setLocation] = useState('')
+  const [website, setWebsite] = useState('')
+  const [structure, setStructure] = useState('')
+
+  const formData = {
+    name: name,
+    location: location,
+    website: website,
+    structure: structure,
+    user: props.user
+  }
+
+  const handleCreateCompany = async (e) => {
+    e.preventDefault()
+    try {
+      const newCompany = await createCompany(formData)
+      console.log('New Company Data: ', newCompany)
+      navigate('/companies')
+    } catch (error) {
+      throw error
+    }
+  }
+
   return (
-    <h1>[Create Company Page here]</h1>
+    <div className="layout">
+      <Header title='Add a Company' />
+      <CompanyForm
+        name={name}
+        setName={setName}
+        location={location}
+        setLocation={setLocation}
+        website={website}
+        setWebsite={setWebsite}
+        structure={structure}
+        setStructure={setStructure}
+        handleCreateCompany={handleCreateCompany}
+      />
+    </div>
   )
 }
 
