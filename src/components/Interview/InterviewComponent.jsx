e40a99b7f6510bab35ed00ccb83b406d4e317927
Abdul2067 <abdul2067@gmail.com>
2021-12-22 16:03:04 -0500

    fill InterviewComponent.jsx

--- src/components/Interview/InterviewComponent.jsx
+++ src/components/Interview/InterviewComponent.jsx
@@ -1,5 +1,53 @@
-import React, { useSate } from "react"
+import React, { useSate, useState } from "react"
 
 import InterviewList from "./InterviewList"
 import CreateInterview from "./CreateInterview"
 
+import * as companyService from "../../services/companyService"
+
+const InterviewComponent = (props) => {
+  const [toggleNew, setToggleNew] = useState(false)
+
+  const handleCreateInterview = async (formData) => {
+    try {
+      const updatedCompany = await 
+      props.handleUpdatedCompany(updatedCompany)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  const handleDeleteInterview = async (interviewId) => {
+    try {
+      await companyService.deleteInterview(props.company._id, interviewId)
+      props.setInterviews(props.interviews.filter(interview => interview._id !== interviewId))
+    } catch (error) {
+      throw error
+    }
+  }
+
+  return (
+    <div className="interview-section">
+      <div className="header">
+        <h3>Interviews</h3>
+      </div>
+      <InterviewList {...props} />
+      <div className="contact-button">
+        {props.user &&
+          <button onClick={() => setToggleNew(!toggleNew)}>Add a Interview</button>
+        }
+      </div>
+      { toggleNew &&
+        <CreateInterview
+          {...props}
+          setToggleNew={setToggleNew}
+          handleCreateInterview={handleCreateInterview}
+          handleDeleteInterview={handleDeleteInterview}
+          handleUpdatedCompany={props.handleUpdatedCompany}
+        />
+      }
+    </div>
+  )
+}
+
+export default InterviewComponent
